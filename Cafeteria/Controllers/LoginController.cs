850b3261d8b471cb455bf6024002f32856c7dab2
jackson.torres.1000 <jackson.torres.1000@gmail.com>
2023-10-31 00:35:09 -0300

    docs: Provisório para Registrar e Logar.

--- Cafeteria/Controllers/LoginController.cs
+++ Cafeteria/Controllers/LoginController.cs
@@ -23,60 +23,60 @@ namespace Cafeteria.Controllers
             return View();
         }
 
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Registro(ClienteRegistroViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var cliente = new Cliente
-                {
-                    UserName = model.Email,
-                    Email = model.Email,
-                    Nome = model.Nome
-                };
+        //[HttpPost]
+        //[AllowAnonymous]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Registro(ClienteRegistroViewModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var cliente = new Cliente
+        //        {
+        //            UserName = model.Email,
+        //            Email = model.Email,
+        //            Nome = model.Nome
+        //        };
 
-                var result = await UserManager.CreateAsync(cliente, model.Senha);
+        //        var result = await UserManager.CreateAsync(cliente, model.Senha);
 
-                if (result.Succeeded)
-                {
-                    await SignInManager.SignInAsync(cliente, isPersistent: false);
-                    // Redirecionar para a página desejada após o registro bem-sucedido
-                    return RedirectToAction("Index", "Home");
-                }
+        //        if (result.Succeeded)
+        //        {
+        //            await SignInManager.SignInAsync(cliente, isPersistent: false);
+        //            // Redirecionar para a página desejada após o registro bem-sucedido
+        //            return RedirectToAction("Index", "Home");
+        //        }
 
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
-            }
+        //        foreach (var error in result.Errors)
+        //        {
+        //            ModelState.AddModelError(string.Empty, error.Description);
+        //        }
+        //    }
 
-            // Se o registro falhar, retorne a mesma página com erros
-            return View(model);
-        }
+        //    // Se o registro falhar, retorne a mesma página com erros
+        //    return View(model);
+        //}
 
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Login(ClienteLoginViewModel model, string returnUrl = null)
-        {
-            if (ModelState.IsValid)
-            {
-                var result = await SignInManager.PasswordSignInAsync(model.Email, model.Senha, isPersistent: false, lockoutOnFailure: false);
+        //[HttpPost]
+        //[AllowAnonymous]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Login(ClienteLoginViewModel model, string returnUrl = null)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var result = await SignInManager.PasswordSignInAsync(model.Email, model.Senha, isPersistent: false, lockoutOnFailure: false);
 
-                if (result.Succeeded)
-                {
-                    // Redirecionar para a página desejada após o login bem-sucedido
-                    return LocalRedirect(returnUrl);
-                }
+        //        if (result.Succeeded)
+        //        {
+        //            // Redirecionar para a página desejada após o login bem-sucedido
+        //            return LocalRedirect(returnUrl);
+        //        }
 
-                ModelState.AddModelError(string.Empty, "Falha na autenticação.");
-            }
+        //        ModelState.AddModelError(string.Empty, "Falha na autenticação.");
+        //    }
 
-            // Se a autenticação falhar, retorne a mesma página com erros
-            return View(model);
-        }
+        //    // Se a autenticação falhar, retorne a mesma página com erros
+        //    return View(model);
+        //}
 
     }
 }
