1bdf159186beeca84e1d1682e4a87919c49a0349
jackson.torres.1000 <jackson.torres.1000@gmail.com>
2023-11-03 15:19:19 -0300

    config: Incluindo o ProdutoService e ProdutoRepository.

--- Cafeteria/Program.cs
+++ Cafeteria/Program.cs
@@ -6,8 +6,9 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Cafeteria;
 using Microsoft.AspNetCore.Builder;
-//using Cafeteria.Services.Interfaces;
-//using Cafeteria.Services.Implementations;
+using Microsoft.Extensions.FileProviders;
+using Cafeteria.Services.Interfaces;
+using Cafeteria.Services.Implementations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,9 +17,10 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddMvc();
 
 builder.Services.AddDbContext<CafeteriaContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("CafeteriaContext")));
-
-//builder.Services.AddScoped<IProdutoService, ProdutoService>();
-//builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+builder.Services.AddSingleton<IFileProvider>(
+    new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")));
+builder.Services.AddScoped<IProdutoService, ProdutoService>();
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
 
 builder.Services.AddHsts(options => {
     options.MaxAge = TimeSpan.FromDays(30);
@@ -31,10 +33,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseDeveloperExceptionPage();
 
 app.UseHttpsRedirection();
@@ -46,11 +49,16 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 
-app.MapDefaultControllerRoute();
+//app.MapDefaultControllerRoute();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Produtos}/{action=Index}/{id?}"
+    );
+    
+});
 
 
 app.Run();
