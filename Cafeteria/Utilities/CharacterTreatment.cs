2294a5ea87d5c9449a988c840be23d6ec8d2dd62
jackson.torres.1000 <jackson.torres.1000@gmail.com>
2023-11-07 20:21:43 -0300

    feat: Incluindo os Utilitarios publicos

---
+++ Cafeteria/Utilities/CharacterTreatment.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace Cafeteria.Utilities
+{
+    public class CharacterTreatment
+    {
+        public static string RemoveDiacritics(string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
