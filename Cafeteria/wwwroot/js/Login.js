1d5b8746578b3db3c8cc5418559ef40b7667fdce
jackson.torres.1000 <jackson.torres.1000@gmail.com>
2023-11-07 20:37:33 -0300

    feat: Incluindo tooltip

--- Cafeteria/wwwroot/js/Login.js
+++ Cafeteria/wwwroot/js/Login.js
@@ -1,1 +1,5 @@
-﻿
+﻿$(document).ready(function () {
+    // tooltips
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+})
