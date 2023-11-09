350b2aada4b95b957ccaa45cb018ffbdfa6ab0a7
jackson.torres.1000 <jackson.torres.1000@gmail.com>
2023-11-09 10:07:33 -0300

    feat: Botão de quantidade.

--- Cafeteria/wwwroot/js/ProdutosDetalhes.js
+++ Cafeteria/wwwroot/js/ProdutosDetalhes.js
@@ -1,3 +1,16 @@
 ﻿$(document).ready(function () {
     $("#backButton").show()
+    $("#increase").click(function () {
+        var quantity = parseInt($("#quantity").val());
+        quantity++;
+        $("#quantity").val(quantity);
+    });
+
+    $("#decrease").click(function () {
+        var quantity = parseInt($("#quantity").val());
+        if (quantity > 1) {
+            quantity--;
+            $("#quantity").val(quantity);
+        }
+    });
 });
