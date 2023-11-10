d5202200c5599b930d089e5b3e4c73a8173acde1
jackson.torres.1000 <jackson.torres.1000@gmail.com>
2023-11-10 19:23:48 -0300

    config: alteração no banco de dados, relacionado a tabelas.

---
+++ Cafeteria/Migrations/20231110211910_IncluidePedido.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cafeteria.Migrations
+{
+    public partial class IncluidePedido : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pedidos_Produtos_ProdutoId",
+                table: "Pedidos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pedidos_ProdutoId",
+                table: "Pedidos");
+
+            migrationBuilder.DropColumn(
+                name: "ProdutoId",
+                table: "Pedidos");
+
+            migrationBuilder.DropColumn(
+                name: "Quantidade",
+                table: "Pedidos");
+
+            migrationBuilder.CreateTable(
+                name: "PedidoProdutos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PedidoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProdutoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Quantidade = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PedidoProdutos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PedidoProdutos_Pedidos_PedidoId",
+                        column: x => x.PedidoId,
+                        principalTable: "Pedidos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PedidoProdutos_Produtos_ProdutoId",
+                        column: x => x.ProdutoId,
+                        principalTable: "Produtos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PedidoProdutos_PedidoId",
+                table: "PedidoProdutos",
+                column: "PedidoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PedidoProdutos_ProdutoId",
+                table: "PedidoProdutos",
+                column: "ProdutoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PedidoProdutos");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProdutoId",
+                table: "Pedidos",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantidade",
+                table: "Pedidos",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedidos_ProdutoId",
+                table: "Pedidos",
+                column: "ProdutoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pedidos_Produtos_ProdutoId",
+                table: "Pedidos",
+                column: "ProdutoId",
+                principalTable: "Produtos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
