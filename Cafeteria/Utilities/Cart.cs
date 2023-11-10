c0df0c367726af459b449ec5868c734cb7c65ffb
jackson.torres.1000 <jackson.torres.1000@gmail.com>
2023-11-10 19:22:56 -0300

    feat: Adicionando a classe Cart para ir incluindo no carrinho

---
+++ Cafeteria/Utilities/Cart.cs
@@ -0,0 +1,46 @@
+﻿using Cafeteria.Models;
+
+namespace Cafeteria.Utilities
+{
+    public class Cart
+    {
+        private List<CartItem> _items = new List<CartItem>();
+
+        public decimal Total => _items.Sum(i => i.Product.Preco * i.Amount);
+
+        public IEnumerable<CartItem> Items => _items.AsReadOnly();
+
+        public int Count => _items.Count;
+
+
+
+        public void AddItem(Produto product, int amount)
+        {
+            var item = _items.FirstOrDefault(i => i.Product.Id == product.Id);
+            if (item == null)
+            {
+                _items.Add(new CartItem { Product = product, Amount = amount });
+            }
+            else
+            {
+                item.Amount += amount;
+            }
+        }
+
+        public void RemoveItem(int productId)
+        {
+            _items.RemoveAll(i => i.Product.Id == productId);
+        }
+
+        public void Clear()
+        {
+            _items.Clear();
+        }
+    }
+    public class CartItem
+    {
+        public Produto Product { get; set; }
+        public int Amount { get; set; }
+    }
+
+}
