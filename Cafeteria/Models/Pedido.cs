b56ea74229e697d3e39be3183654d85583449d95
jackson.torres.1000 <jackson.torres.1000@gmail.com>
2023-11-10 19:22:29 -0300

    chore: Removendo quantidade e produto da tabela e classe

--- Cafeteria/Models/Pedido.cs
+++ Cafeteria/Models/Pedido.cs
@@ -7,28 +7,25 @@ namespace Cafeteria.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
         [Display(Name = "Cliente")]
         public int ClienteId { get; set; }
-        [Required]
-        [Display(Name = "Produto")]
-        public int ProdutoId { get; set; }
+        
         [Required]
         [Display(Name = "Administrador")]
         public int AdministradorId { get; set; }
-        [Required]
-        [Range(1, 1000)]
-        [Display(Name = "Quantidade")]
-        [RegularExpression(@"^[0-9]*$", ErrorMessage = "Apenas números são permitidos.")]
-        [DataType(DataType.Text)]
-        public int Quantidade { get; set; }
+        
         public string? Status { get; set; }
+        
         public DateTime? DataPedido { get; set; }
+        
         [ForeignKey("ClienteId")]
         public Cliente? Cliente { get; set; }
-        [ForeignKey("ProdutoId")]
-        public Produto? Produto { get; set; }
+        
         [ForeignKey("AdministradorId")]
         public Administrador? Administrador { get; set; }
+
+        public ICollection<PedidoProduto>? PedidoProdutos { get; set; }
     }
 }
