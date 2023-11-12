eadf9c243f33ab1e23b003491ccf340c5c967b68
jackson.torres.1000 <jackson.torres.1000@gmail.com>
2023-11-11 21:04:07 -0300

    feat: CSS e JS incluido e no bundle

---
+++ Cafeteria/wwwroot/js/PedidoList.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    $(".finish").click(function () {
+        var id = $(this).closest(".div-infor").find(".pedidoId").val();
+        var url = "/Pedidos/Finish/" + id;
+        $.ajax({
+            url: url,
+            type: "POST",
+            success: function (data) {
+                if (data.success) {
+                    alert("Pedido finalizado com sucesso!");
+                    location.reload();
+                }
+                else {
+                    alert("Erro ao finalizar pedido!");
+                }
+            }
+        });
+    })
+})
