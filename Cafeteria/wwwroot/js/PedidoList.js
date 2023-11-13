4e6d416878e6545a904788221b3d58c86e94d3a1
jackson.torres.1000 <jackson.torres.1000@gmail.com>
2023-11-13 11:35:54 -0300

    feat: Incluindo o in-preparation (Em preparo)

--- Cafeteria/wwwroot/js/PedidoList.js
+++ Cafeteria/wwwroot/js/PedidoList.js
@@ -7,7 +7,23 @@
             type: "POST",
             success: function (data) {
                 if (data.success) {
-                    alert("Pedido finalizado com sucesso!");
+                    location.reload();
+                }
+                else {
+                    alert("Erro ao finalizar pedido!");
+                }
+            }
+        });
+    })
+
+    $(".in-preparation").click(function () {
+        var id = $(this).closest(".div-infor").find(".pedidoId").val();
+        var url = "/Pedidos/InPreparation/" + id;
+        $.ajax({
+            url: url,
+            type: "POST",
+            success: function (data) {
+                if (data.success) {
                     location.reload();
                 }
                 else {
