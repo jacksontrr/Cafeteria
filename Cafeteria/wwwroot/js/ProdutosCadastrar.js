c963856ddf5f5246222367a86f5b65cd28f2e0ae
jackson.torres.1000 <jackson.torres.1000@gmail.com>
2023-11-15 02:06:26 -0300

    feat: CSS e JS do produto

---
+++ Cafeteria/wwwroot/js/ProdutosCadastrar.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $("#backButton").show()
+    let imagem = $("#imagem").attr("src")
+    
+    $("#Arquivo").on("change", function () {
+        if (this.files && this.files[0]) {
+            $("#RemoverImagem").parent("span").removeClass("d-flex").addClass("d-none")
+            $("#imagem").attr("src", URL.createObjectURL(this.files[0]))
+            $("#RemoverImagem").prop("checked", false)
+        } else {      
+            $("#imagem").attr("src", imagem)
+        }
+    })
+});
---
+++ Cafeteria/wwwroot/js/ProdutosEditar.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    $("#backButton").show()
+    let imagem = $("#imagem").attr("src")
+    
+    $("#Arquivo").on("change", function () {
+        // verificar se tem alguma coisa
+        if (this.files && this.files[0]) {
+            $("#RemoverImagem").parent("span").removeClass("d-flex").addClass("d-none")
+            $("#imagem").attr("src", URL.createObjectURL(this.files[0]))
+            $("#RemoverImagem").prop("checked", false)
+        } else {
+            $("#RemoverImagem").parent("span").addClass("d-flex").removeClass("d-none")
+            $("#imagem").attr("src", imagem)
+        }
+    })
+
+    $("#RemoverImagem").on("click", function () {
+        if ($(this).is("checked")) {
+            $(this).attr("checked", true)
+        } else {
+            $(this).attr("checked", false)
+        }
+        //$("#Arquivo").val("")
+        //$("#removerImagem").parent("span").addClass("d-none").removeClass("d-flex")
+        //$("#imagem").attr("src", "")
+    })
+});
